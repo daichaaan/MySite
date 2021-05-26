5ff5c3024f3bcff12ee4f5888f4b1fc2a9c91a6c
daichaaan <82603351+daichaaan@users.noreply.github.com>
2021-05-26 19:24:50 +0900

    mysite up

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function setup()
-{
-    add_theme_support('post-thumbnails'); // アイキャッチ画像を有効化
-    add_theme_support('title-tag'); // タイトルタグ自動生成
-}
-
-add_action('after_setup_theme', 'setup');
